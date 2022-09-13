e81396feb3dce146088a99461c47c12b212d315d
muirglacier <69924699+muirglacier@users.noreply.github.com>
2022-09-14 08:27:54 +1000

    clean

--- debugconf.sh
+++
@@ -1,1 +0,0 @@
-./configure CXXFLAGS='-O3 -g' --enable-static --prefix=/home/anonymous/ain/depends/x86_64-w64-mingw32 --disable-zmq
--- normalconf.sh
+++
@@ -1,2 +0,0 @@
-export BDB_PREFIX='/home/anonymous/Development/ain/db4'
-./configure --prefix="$(pwd)/depends/x86_64-pc-linux-gnu" CXXFLAGS="-O3 -march=native" CFLAGS="-O3 -march=native" BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --enable-static
