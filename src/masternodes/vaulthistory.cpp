62a38f202318340134acd1953a565001c3d405f3
muirglacier <69924699+muirglacier@users.noreply.github.com>
2022-09-17 15:51:51 +1000

    BAM

--- src/masternodes/vaulthistory.cpp
+++ src/masternodes/vaulthistory.cpp
@@ -27,7 +27,7 @@ std::vector<VaultStruct>CVaultHistoryView::GetVaultHistoryHeight(uint32_t height
 
     auto it = LowerBound<ByVaultHistoryKey>(VaultHistoryKey{height});
     for (; it.Valid() && it.Key().blockHeight == height; it.Next()) {
-        const auto stateKey = VaultStateKey{key.vaultID, key.blockHeight};
+        const auto stateKey = VaultStateKey{it.Key().vaultID, it.Key().blockHeight};
         const auto state = ReadBy<ByVaultStateKey>(stateKey);
         history.push_back(VaultStruct{it.Key(), it.Value(), state});
     }
