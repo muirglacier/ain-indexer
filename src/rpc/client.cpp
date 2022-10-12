14f9defe9d9b23b6920413cb9f818128f10b2b9a
muirglacier <69924699+muirglacier@users.noreply.github.com>
2022-10-12 10:39:04 +1000

    Update client.cpp

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -302,6 +302,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "listpoolshares", 2, "is_mine_only" },
 
     { "listaccounthistory", 1, "options" },
+    { "listaccountrewards", 1, "options" },
     { "getaccounthistory", 1, "blockHeight" },
     { "getaccounthistory", 2, "txn" },
     { "listburnhistory", 0, "options" },
